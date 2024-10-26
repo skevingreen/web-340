e83e2ab135870839689f17c1722ed9e35be8170d
Scott Green <skevingreen@gmail.com>
2024-10-26 13:20:39 -0700

    completed assignment 1.3

--- week-1/weight-converter/weight-converter.js
+++ week-1/weight-converter/weight-converter.js
@@ -1,10 +1,29 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author: Scott Green
+ * Date: October 26, 2024
+ * File Name: weight-converter.js
+ * Description: Converts pounds to kilograms
+ */
 
 "use strict";
 
-// TODO: Implement the weight conversion logic here
+// Converts pounds to kilograms
+function main() {
+  // Display an error message and exit program if the required command line argument is not provided (or if there is more than one command line argument provided)
+  if (process.argv.length != 3) {
+    console.error("Usage: node weight-converter.js <pounds>");
+    process.exit(1);
+  }
+
+  // Display an error message and exit program if the provided command line argument is not a number
+  if (isNaN(process.argv[2])) {
+    console.error("Input must be a number.");
+    process.exit(1);
+  }
+
+  // Display the conversion of pounds to kilograms to two decimal places
+  console.log( (parseFloat(process.argv[2]) * 0.45359237).toFixed(2) );
+}
+
+// Execute the main function
+main();
