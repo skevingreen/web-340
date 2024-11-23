f691ea8c938ba5935a383b6e51163c4f24d7e8c1
Scott Green <skevingreen@gmail.com>
2024-11-23 09:01:15 -0700

    completed chapter 5 exercise

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,38 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Scott Green
+ * Date: November 22, 2024
+ * File Name: pie.js
+ * Description: A function for baking a pie when given pie type and ingredients
  */
 "use strict";
 
 function bakePie(pieType, ingredients) {
   // Your code here
+  let essentialIngredients = ["flour", "sugar", "butter"];  // required ingredients
+  let pieBaked = true;  // flag to tell if pie was baked or not
+
+  // Check that in addition to the essential ingredients there is at least one more
+  if (ingredients.length < 4) {
+    console.log("You need at least 4 ingredients to bake a pie - flour, sugar, butter, and a filling");
+    pieBaked = false; // missing something so set flag to false
+  }
+
+  // Check that each of the essential ingredients is among the passed in ingredient list
+  for (let ingredient of essentialIngredients) {
+    if (!ingredients.includes(ingredient)) {
+      // One of the essential ingredients is missing so log a message and exit the program
+      console.log("An essential ingredient (flour, sugar, or butter) is missing.");
+      process.exit(1);
+    }
+  }
+
+  // Return an appropriate message for whether the pie was baked or not
+  if (pieBaked) {
+    return pieType + " pie has been baked";
+  } else {
+    return pieType + " pie has NOT been baked";
+  }
 }
 
+// Export the bakePie function
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,12 +1,54 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Scott Green
+ * Date: November 22, 2024
+ * File Name: pie.spec.js
+ * Description: Test Suite For the bakePie Function in pie.js
  */
 
 "use strict";
 
+// Import bakePie function from pie.js
 const { bakePie } = require("../src/pie");
 
+// Monitor the exit method of the process object so we can tell what exit code was used
+const exit = jest.spyOn(process, 'exit').mockImplementation((code) => code);
+
 // Your tests here
+
+// Create the Jest test suite
+describe("isBaked", ()=> {
+    let pieStatus;  // Variable to hold the status returned from bakePie()
+    let log;        // Variable for the mocked console log
+
+    // Before each test, mock the log
+    beforeEach(()=>{
+        log = jest.spyOn(console, "log");
+    });
+
+    // After each test, restore the log to normal
+    afterEach(()=>{
+        log.mockRestore();
+    });
+
+    // Test unsuccessful, but non-fatal, return message.
+    // bakePie is called with all essential ingredients for the crust but no filling.
+    test("cherry pie status", () => {
+        pieStatus = bakePie("cherry", ["butter", "flour", "sugar"]);
+        expect(pieStatus).toEqual("cherry pie has NOT been baked");
+        expect(exit).not.toHaveBeenCalled();
+    });
+
+    // Test a successful call to bakePie
+    test("lemon pie status", () => {
+        pieStatus = bakePie("lemon", ["flour", "lemon", "sugar", "butter", "cornstarch"]);
+        expect(pieStatus).toEqual("lemon pie has been baked");
+        expect(exit).not.toHaveBeenCalled();
+    });
+
+    // Test calling bakePie with missing essential ingredient (fatal error)
+    test("pecan pie status", () => {
+        pieStatus = bakePie("pecan", ["flour", "pecan", "butter"]);
+        expect(log).toHaveBeenCalledWith("An essential ingredient (flour, sugar, or butter) is missing.");
+        expect(exit).toHaveBeenCalledWith(1);
+    });
+});
