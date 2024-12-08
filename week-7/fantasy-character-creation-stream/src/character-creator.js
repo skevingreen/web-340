4fb61e54bc2ed50af130b4f3f90d6f1cac81494d
Scott Green <skevingreen@gmail.com>
2024-12-08 08:34:52 -0700

    completed chapter 7 programming exercise

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,18 +1,53 @@
-const { Duplex } = require('stream');
+/**
+ * Author: Scott Green
+ * Date: December 7, 2024
+ * File Name: character-creator.js
+ * Description: Duplex Stream Processor for Character Generator
+ */
+
+"use strict";
+
+const {Duplex} = require('stream');
 
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
-    // TODO: Initialize your class here
+    // Set up a queue for reading/writing character information to
+    this.queue = [];
   }
 
   _write(chunk, encoding, callback) {
-    // TODO: Implement your _write method here
+    // Put the data in a constant string
+    const character = chunk.toString().trim();
+
+    // If the character data is bad, log an error
+    if (!character) {
+      callback(new Error("Invalid character data."));
+      return;
+    } else {
+      // Otherwise, parse the data and push the transformed results to the stream
+      const characterJSON = JSON.parse(character);
+
+      const characterClass = characterJSON.characterClass;
+      const gender = characterJSON.gender;
+      const funFact = characterJSON.funFact;
+
+      this.queue.push(`The character class is: ${characterClass}    The character gender is: ${gender}    A fun fact about the character: ${funFact}`);
+
+      callback();
+    }
   }
 
   _read(size) {
-    // TODO: Implement your _read method here
+    // If we have character data in the queue, read it
+    if(this.queue.length) {
+      this.push(this.queue.shift() + "\n");
+    } else {
+      // Otherwise push null to stream
+      this.push(null);
+    }
   }
 }
 
+// Export the class
 module.exports = CharacterCreator;
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -1,3 +1,12 @@
+/**
+ * Author: Scott Green
+ * Date: December 7, 2024
+ * File Name: character-creator.spec.js
+ * Description: Test Suite for Stream Character Generator
+ */
+
+"use strict";
+
 const CharacterCreator = require('../src/character-creator');
 
 describe('CharacterCreator', () => {
@@ -8,14 +17,54 @@ describe('CharacterCreator', () => {
   });
 
   test("should process data correctly when written to", (done) => {
-    // TODO: Write your test here
+    // Create an array of characters
+    const characters = [
+      {"characterClass":"Warrior", "gender":"Male", "funFact":"Has one eye."},
+      {"characterClass":"Rouge", "gender":"Female", "funFact":"Has a peg leg."}
+    ]
+
+    // Write each character to the stream
+    characters.forEach( character => {
+      characterCreator.write(JSON.stringify(character));
+    });
+
+    // Process a character
+    characterCreator.on('data', (data) => {
+      const character = characters.shift();
+
+      const characterClass = character.characterClass;
+      const gender = character.gender;
+      const funFact = character.funFact;
+
+      expect(data.toString().trim()).toBe(`The character class is: ${characterClass}    The character gender is: ${gender}    A fun fact about the character: ${funFact}`)
+
+      if(characters.length === 0) {
+        done();
+      }
+    });
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
-    // TODO: Write your test here
+    // Create an invalid character
+    characterCreator.write("");
+
+    // Process error
+    characterCreator.on("error", (err) => {
+      expect(err.message).toBe("Invalid character data.");
+      done();
+    });
   });
 
   test("should transform data correctly when written to", (done) => {
-    // TODO: Write your test here
+    // Create a character
+    const character = '{"characterClass":"Mage","gender":"Female","funFact":"Has purple hair."}';
+
+    // Process the character
+    characterCreator.write(character, 'utf8', () => {
+      characterCreator.on('data', (data) => {
+        expect(data.toString().trim()).toBe("The character class is: Mage    The character gender is: Female    A fun fact about the character: Has purple hair.")
+        done();
+      });
+    });
   });
 });
