99ce252e3366d0991592aeb34b52fef328380d5f
Scott Green <skevingreen@gmail.com>
2024-12-14 16:04:30 -0700

    completed programming exercise for chapter 8

--- week-8/fantasy-character-creation/src/character-creation.js
+++ week-8/fantasy-character-creation/src/character-creation.js
@@ -1,5 +1,12 @@
 "use strict";
 
+/**
+ * Author: Scott Green
+ * Date: December 10, 2024
+ * File Name: character-creation.js
+ * Description:
+ */
+
 /*
  * This file allows you to choose between using callbacks or promises (async/await) for handling asynchronous operations.
  *
@@ -15,32 +22,47 @@
  */
 
 // For callbacks:
-/*
+
 const fs = require('fs');
+const {join} = require('path');
+
+const CHARACTER_OUTPUT_FILE = join(__dirname, "character_file_out.txt");
+const CHARACTER_INPUT_FILE = join(__dirname, "character_file_in.txt");
 
 function createCharacter(character, callback) {
   // TODO: Implement this function
+  const data = character;
+
+  fs.writeFile(CHARACTER_OUTPUT_FILE, data, (err) => {
+    callback(err);
+  });
 }
 
 function getCharacters(callback) {
   // TODO: Implement this function
+  fs.readFile(CHARACTER_INPUT_FILE, {encoding: "utf8"}, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      const character_read = data;
+      callback(null, character_read);
+    }
+  });
 }
-*/
+
 
 // For promises:
-/*
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
 
-async function createCharacter(character) {
-  // TODO: Implement this function
-}
+// async function createCharacter(character) {
+//   // TODO: Implement this function
+// }
 
-async function getCharacters() {
-  // TODO: Implement this function
-}
-*/
+// async function getCharacters() {
+//   // TODO: Implement this function
+// }
 
 // Uncomment the appropriate exports depending on whether you're using callbacks or promises:
 
-// module.exports = { createCharacter, getCharacters }; // For callbacks
+ module.exports = { createCharacter, getCharacters }; // For callbacks
 // module.exports = { createCharacter, getCharacters }; // For promises
--- week-8/fantasy-character-creation/test/character-creation.spec.js
+++ week-8/fantasy-character-creation/test/character-creation.spec.js
@@ -1,6 +1,13 @@
 "use strict";
 
 /**
+ * Author: Scott Green
+ * Date: December 10, 2024
+ * File Name: character-creation.spec.js
+ * Description:
+ */
+
+/**
  * This file allows you to choose between using callbacks or promises (async/await) for handling asynchronous operations.
  *
  * If you want to use callbacks:
@@ -11,7 +18,7 @@
  */
 
 // For callbacks:
-// const fs = require('fs');
+ const fs = require('fs');
 
 // For promises:
 // const fs = require('fs').promises;
@@ -19,15 +26,52 @@
 describe("Character Creation Module", () => {
   let createCharacter;
   let getCharacters;
+  let testCharacter = {"class":"Warrior", "gender":"Male", "funFact":"Looks like Sean Connery"};
 
   beforeEach(() => {
     jest.resetModules();
     // TODO: Set up your mocks here
+
+    // jest.spyOn(fs, 'readFile').mockImplementation( () => Promise.resolve() );
+    // jest.spyOn(fs, 'writeFile').mockImplementation( () => Promise.resolve() );
+
+    jest.spyOn(fs, 'writeFile').mockImplementation((file, data, callback) => callback(null));
+    jest.spyOn(fs, 'readFile').mockImplementation((file, options, callback) => callback(null, testCharacter));
+
     ({ createCharacter, getCharacters } = require('../src/character-creation'));
   });
 
   // TODO: Write your tests here. You should have at least three tests:
   // 1. Test that createCharacter writes a new character to the file
+
+  // test("writes character to file", async () => {
+  //   await expect(createCharacter(testCharacter)).resolves.toBeUndefined();
+  // });
+
+  test("writes character to file", (done) => {
+    createCharacter(testCharacter, (err) => {
+      expect(err).toBeNull();
+      done();
+    });
+  });
+
   // 2. Test that getCharacters reads characters from the file
+  test("reads character data", (done)=> {
+    getCharacters((err, character) => {
+      expect(err).toBeNull();
+      expect(character).toEqual(testCharacter);
+      done();
+    })
+  })
+
   // 3. Test that createCharacter handles errors when writing to the file
+  test("handles errors when writing character to file", (done) => {
+    fs.writeFile.mockImplementationOnce((file, options, callback) => callback(new Error("File not found")));
+
+    createCharacter(testCharacter, (err) => {
+      expect(err).not.toBeNull();
+      expect(err.message).toBe("File not found");
+      done();
+    });
+  });
 });
