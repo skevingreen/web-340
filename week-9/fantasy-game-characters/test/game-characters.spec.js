03ff42aa7be7eed40f28193fbfe3fe9fe3f04c8e
Scott Green <skevingreen@gmail.com>
2024-12-18 17:59:38 -0700

    I seem to have missed the spec file

--- week-9/fantasy-game-characters/test/game-characters.spec.js
+++ week-9/fantasy-game-characters/test/game-characters.spec.js
@@ -1,3 +1,12 @@
+"use strict";
+
+/**
+ * Author: Scott Green
+ * Date: December 16, 2024
+ * File Name: game-characters-spec.js
+ * Description: test suite
+ */
+
 // game-characters.spec.js
 const { GameCharacters } = require("../src/game-characters");
 
@@ -10,13 +19,34 @@ describe("GameCharacters", () => {
 
   test("should return game characters data", (done) => {
     // TODO: Implement this test
+    gameCharacters.getCharacters((data, error) => {
+      expect(error).toBeNull();
+      expect(data).toEqual([
+        {class:"Warrior", gender:"Male", funFact:"plays piano on the weekends"},
+        {class:"Mage", gender:"Female", funFact:"never used a magic missile"},
+        {class:"Rogue", gender:"Female", funFact:"only wears plaid"}
+      ])
+      done();
+    });
   });
 
   test("should handle an error when the game characters data script is not found", (done) => {
     // TODO: Implement this test
+    const gameCharacters = new GameCharacters("nonexistent-script.js");
+    gameCharacters.getCharacters((data, error) => {
+      expect(data).toBeNull();
+      expect(error).not.toBeNull();
+      done();
+    });
   });
 
   test("should handle an error when the game characters data script fails", (done) => {
     // TODO: Implement this test
+    const gameCharacters = new GameCharacters("failing-script.js");
+    gameCharacters.getCharacters((data, error) => {
+      expect(data).toBeNull();
+      expect(error).not.toBeNull();
+      done();
+    })
   });
 });
